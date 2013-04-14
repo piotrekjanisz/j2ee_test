b8f4f8c830bbde89a2ac7a0a75b21d06476df37f
Piotrek <piotrek@piotrek-laptop.(none)>
2013-04-14 22:47:42 +0200

    Added web project.

--- movies/dao/src/test/java/pl/janisz/movies/dao/repositories/MovieRepositoryTest.java
+++ movies/dao/src/test/java/pl/janisz/movies/dao/repositories/MovieRepositoryTest.java
@@ -3,7 +3,6 @@ package pl.janisz.movies.dao.repositories;
 import static org.junit.Assert.*;
 
 import java.util.Iterator;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
